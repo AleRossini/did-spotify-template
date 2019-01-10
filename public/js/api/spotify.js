76e3ccc3cbdc7c15827468e427c92d592f901f3c
Alessia <alessia.rossini.91@gmail.com>
2019-01-10 13:31:07 +0000

    new: babel, css | achieved: get info from spotify API, get artists object, get 10 top tracks | work-in-P: graphic, get track selected by user and compare danceability value

--- public/js/api/spotify.js
+++ public/js/api/spotify.js
@@ -6,8 +6,16 @@ const getUserData = (accessToken) => {
   }).then(response => response.json())
 };
 
-const getSongs = (accessToken) => {
-  return fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks`, {
+const getSongs = (accessToken, id) => {
+  return fetch(`https://api.spotify.com/v1/artists/${id}/top-tracks?country=IE`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+  }).then(response => response.json())
+};
+
+const getTrackDetails = (accessToken, id) => {
+  return fetch(`https://api.spotify.com/v1/audio-features/${id}`, {
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
@@ -20,10 +28,12 @@ const getArtists = (accessToken, artist) => {
         'Authorization': `Bearer ${accessToken}`
       }
   }).then(response => response.json())
+  .then(response => response.artists.items[0])
 };
 
 export default {
   getUserData,
   getSongs,
-  getArtists
+  getArtists,
+  getTrackDetails
 }
--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -9,14 +9,14 @@ const {access_token, state} = getHashParams();
 const storedState = localStorage.getItem(STATE_KEY);
 
 
-const outputTemplate = ({display_name, id, email, uri, external_urls, images, country}) =>`<h1>Logged in as </h1>
-  <div class="media">
+const outputTemplate = ({display_name, id, email, uri, external_urls, images, country}) =>`<h1>Hey <b>${display_name}</b>, <br/> do you know who's the best ...??</h1>
+  <!--div class="media">
     <div class="pull-left">
       <img class="media-object" width="150" src="">
     </div>
     <div class="media-body">
       <dl class="dl-horizontal">
-        <dt>Display name</dt><dd class="clearfix">${display_name}</dd>
+        <dt>Display name</dt><dd class="clearfix"></dd>
         <dt>Id</dt><dd>${id}</dd>
         <dt>Email</dt><dd>${email}</dd>
         <dt>Spotify URI</dt><dd><a href="${uri}">${uri}</a></dd>
@@ -25,8 +25,7 @@ const outputTemplate = ({display_name, id, email, uri, external_urls, images, co
         <dt>Country</dt><dd>${country}</dd>
       </dl>
     </div>
-    <a type ="button" href="">Go to the Battle!</a>
-  </div>`
+  </div-->`
 
 
 if (!access_token || (state == null || state !== storedState)) {
@@ -48,33 +47,73 @@ if (!access_token || (state == null || state !== storedState)) {
  //
  //   console.log(value);
  // }
+async function getSongsForArtist() {
+  var artistsName = document.getElementById('typedArtistName').value.toLowerCase();
+  var artist = await SpotifyAPI.getArtists(access_token, artistsName);
+  var songs = await SpotifyAPI.getSongs(access_token, artist.id);
+  // var checkedSong = 'valore iniziale';
 
-document.getElementById('sendArtistName').addEventListener('click', () => {
-      var artistsName = document.getElementById('typedArtistName').value.toLowerCase();
-      console.log(artistsName);
-      // return artistsName;
-       var artistsInfo = SpotifyAPI.getArtists(access_token, artistsName).then(data => {console.log(data)});
-       // var artistId = artistsInfo.artists.id;
-       console.log(artistId);
-});
-
- // function getArtistsName() {
- //   var artistsName = document.getElementById('typedArtistName').value.toLowerCase();
- //   console.log(artistsName);
- //   return artistsName;
- // };
-
- // var artistsList = SpotifyAPI.getArtists(access_token, getArtistsName).then(data => {outputTemplate(data);});
- // console.log(artistsName);
- // console.log(artistsList);
- // // var artistId =
- // var songsList = SpotifyAPI.getSongs(access_token, id).then(data => {outputTemplate(data);});
-
-
-
+    songs.tracks.forEach((song) => {
+      var list = document.createElement('li');
+      list.appendChild(document.createTextNode(song.name));
+      var input = document.createElement('input');
+      input.class = 'song';
+      input.type = "radio";
+      input.name = "song";
+      input.value = song.id;
+        // input.addEventListener('change', function prova() {
+        //   input.checked ? (checkedSong = input.value) : null;
+        //   console.log(checkedSong);
+        // })
+      document.getElementById('artistTopTracks').appendChild(list).appendChild(input);
+    })
+    // prova();
+    // console.log(checkedSong);
 
 
+    // Trying to solve with FILTER
+    // const radioButtons = document.getElementsByName("song");
+    // const button = radioButtons.filter(function(button) {
+    // return button.checked;
+    // })[0]
 
- // console.log(value);
+    // Trying to solve with FIND
+    var radioButtons = document.getElementsByName("song");
+    // radioButtons.addEventListener('change', (button) => { console.log(button.value)});
+    // console.log(radioButtons);
+    // radioButtons.addEventListener('change', () => {
+      const songsList = Array.from(radioButtons);
+      var checkedSong = songsList.find(button => button.checked === true);
+    // })
 
-// var song1 = document.getElementById("song1").innerHTML =
+    console.log(radioButtons);
+    console.log(songsList);
+    console.log(checkedSong);
+
+
+    // Create a static list
+    // var songsList = document.querySelectorAll( 'input[name=song]');
+
+    //  Create a live/dynamic list
+    // var radioButtons = document.getElementsByName("song");
+
+    // Using click() to watch on radio button
+    // var checked = songsList.click();
+
+    //  Can't remember what this does
+    // var checked = Array.prototype.forEach.call(songsList, function (item) {
+    //   item.checked = true;
+    // });
+
+    // Next step -> Take info of the SELECTED/CHECKED track
+    // if ( button.checked === true) {
+    //   var trackInfo = await SpotifyAPI.getTrackDetails(access_token, button.id, songs.tracks[0].id);
+    //   console.log(trackInfo);
+    // }
+    // else {
+    //
+    // }
+
+}
+
+document.getElementById('sendArtistName').addEventListener('click', getSongsForArtist);
